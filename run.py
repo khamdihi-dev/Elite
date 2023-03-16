8f04699e02395a11ea68038425e20151378e7c1b
KhamdihiXD <118111949+khamdihi-dev@users.noreply.github.com>
2023-03-16 13:56:26 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from os import system
 from proyek import run
 
+def main():
+    system('git pull')
+    run.licensi().cek()
 
-system('git pull')
-run.licensi().cek()
+if __name__ == '__main__':main()
